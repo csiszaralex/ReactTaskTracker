a367ed96e94ddcb2abc5d5b0837217f2aee35ad5
Alex <csiszaralex@gmail.com>
2021-03-22 23:26:33 +0100

    :sparkles: Add toggle to reminder

--- src/App.js
+++ src/App.js
@@ -27,11 +27,18 @@ const App = () => {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)));
+  };
 
   return (
     <div className="container">
       <Header title="Hello React!" />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : "No Tasks To Show"}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 };

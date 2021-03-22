32afb824ba15f81dcd201bf6a4de9390d8aa9dc3
Alex <csiszaralex@gmail.com>
2021-03-22 22:59:29 +0100

    :sparkles: Add Tasks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import Header from "./components/Header";
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import Tasks from "./components/Tasks.jsx";
 
 const App = () => {
   const name = "Alex";
   const felk = true;
 
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Asd",
+      day: "ma",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Mááás",
+      day: "holnap",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "xDD",
+      day: "jövőre",
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className="container">
-      <Header title="Hello React!"/>
+      <Header title="Hello React!" />
       <h2>
         Hello {name}
         {felk ? "!" : ""}
       </h2>
+      <Tasks tasks={tasks} />
     </div>
   );
 };
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,11 @@
+function Tasks({ tasks }) {
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
+}
+
+export default Tasks;
